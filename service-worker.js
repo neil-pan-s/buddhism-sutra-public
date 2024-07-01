5c1199e205dbf4a1b3daec763488d25f4fa825aa
neil.pan <neil.pan@qq.com>
2024-07-01 20:53:13 +0800

    first commit

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sutra/precache-manifest.4023e396c6ea8d25821f66bb0d713fc2.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "buddhism-sutra"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
---
+++ sutras/药师佛心咒/药师佛心咒.js
@@ -0,0 +1,28 @@
+window.__buddhism_sutra = {
+  "text": `
+die ya ta     
+喋雅他 
+
+ong
+嗡 
+
+bei kan ze bei kan ze
+贝堪则 贝堪则 
+
+ma ha bei kan ze
+玛哈贝堪则 
+
+la za sa mu 
+喇杂萨目 
+
+ga die
+嘎喋 
+
+so ha 
+梭哈
+
+  `,
+  "tip": `
+  在密宗佛教中特别是东密佛教有以石碑护身符制作符咒保护别人平安健康。药师佛心咒：石碑护身符组成“红蚕石、靛蓝晶、影子石、蜜蜡石、云海石”。药师佛心咒石碑护身符可以保护平安健康，化解人生中的凶煞。
+  `,
+}
